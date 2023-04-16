203b04717e91b43e74b7647f88dac77c9141f728
Yasmine Benali <128916490+Yugenx@users.noreply.github.com>
2023-04-16 21:50:14 +0200

    Update GrapheLArcs.java

--- GrapheLArcs.java
+++ GrapheLArcs.java
@@ -1,52 +1,43 @@
 package Graphe;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
-
 
 public class GrapheLArcs implements IGraphe {
+
     private List<Arc> arcs;
 
     public GrapheLArcs() {
         arcs = new ArrayList<>();
     }
 
-    public GrapheLArcs(String str) {
-        arcs = new ArrayList<>();
-        peupler(str);
-    }
-
     @Override
-    public void ajouterSommet(String noeud) { 
-    
-    if (!contientSommet(noeud)) {
-            // On ajoute un arc qui part et arrive sur le sommet pour le créer
+    public void ajouterSommet(String noeud) {
+        if (!contientSommet(noeud)) {
             arcs.add(new Arc(noeud, noeud, 0));
         }
-       
     }
 
     @Override
     public void ajouterArc(String source, String destination, Integer valeur) {
-         if (valeur < 0) {
-            throw new IllegalArgumentException("La valuation d'un arc ne peut pas être négative.");
+        if (valeur == null || valeur < 0) {
+            throw new IllegalArgumentException("La valuation doit être positive.");
         }
-        if (!contientSommet(source)) {
-            ajouterSommet(source);
+        if (!contientSommet(source) || !contientSommet(destination)) {
+            throw new IllegalArgumentException("Les sommets source et destination doivent exister dans le graphe.");
         }
-        if (!contientSommet(destination)) {
-            ajouterSommet(destination);
+        if (contientArc(source, destination)) {
+            throw new IllegalArgumentException("L'arc (" + source + ", " + destination + ") existe déjà.");
         }
         arcs.add(new Arc(source, destination, valeur));
-    }
-        
     }
 
     @Override
     public void oterSommet(String noeud) {
         arcs.removeIf(arc -> arc.getSource().equals(noeud) || arc.getDestination().equals(noeud));
+    }
 
     @Override
     public void oterArc(String source, String destination) {
@@ -61,9 +52,7 @@ public class GrapheLArcs implements IGraphe {
             sommets.add(arc.getDestination());
         }
         return new ArrayList<>(sommets);
-        }
-       
- 
+    }
 
     @Override
     public List<String> getSucc(String sommet) {
@@ -77,16 +66,16 @@ public class GrapheLArcs implements IGraphe {
     }
 
     @Override
-   public int getValuation(String src, String dest) {
+    public int getValuation(String src, String dest) {
         for (Arc arc : arcs) {
             if (arc.getSource().equals(src) && arc.getDestination().equals(dest)) {
                 return arc.getValuation();
             }
         }
         throw new IllegalArgumentException("L'arc (" + src + ", " + dest + ") n'existe pas.");
-        }
+    }
 
-   public boolean contientSommet(String sommet) {
+    public boolean contientSommet(String sommet) {
         for (Arc arc : arcs) {
             if (arc.getSource().equals(sommet) || arc.getDestination().equals(sommet)) {
                 return true;
@@ -105,19 +94,15 @@ public class GrapheLArcs implements IGraphe {
         return false;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Arc arc : arcs) {
+            sb.append(String.format("%s->%s(%d), ", arc.getSource(), arc.getDestination(), arc.getValuation()));
+        }
+        if (sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1); //
+        }
 
-   @Override
-public String toString() {
-    StringBuilder sb = new StringBuilder();
-    for (Arc arc : arcs) {
-        sb.append(String.format("%s-%s(%d), ", arc.getSource(), arc.getDestination(), arc.getValuation()));
-    }
-    if (sb.length() > 0) {
-        sb.deleteCharAt(sb.length() - 1); // supprime la dernière virgule
-        sb.deleteCharAt(sb.length() - 1); // supprime l'espace en fin de chaîne
-    }
-    return sb.toString();
     }
 
-
-}
