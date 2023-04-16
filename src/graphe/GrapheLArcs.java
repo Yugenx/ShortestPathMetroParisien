ebf420c45e4ece3eb6b8a621c31d2b8f937b93ef
Yasmine Benali <128916490+Yugenx@users.noreply.github.com>
2023-04-17 01:22:13 +0200

    Add files via upload

--- src/graphe/GrapheLArcs.java
+++ src/graphe/GrapheLArcs.java
@@ -1,4 +1,4 @@
-package Graphe;
+package graphe;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -25,15 +25,17 @@ public class GrapheLArcs implements IGraphe {
         if (valeur == null || valeur < 0) {
             throw new IllegalArgumentException("La valuation doit être positive.");
         }
-        if (!contientSommet(source) || !contientSommet(destination)) {
-            throw new IllegalArgumentException("Les sommets source et destination doivent exister dans le graphe.");
+        if (!contientSommet(source)) {
+            ajouterSommet(source);
+        }
+        if (!contientSommet(destination)) {
+            ajouterSommet(destination);
         }
         if (contientArc(source, destination)) {
             throw new IllegalArgumentException("L'arc (" + source + ", " + destination + ") existe déjà.");
         }
         arcs.add(new Arc(source, destination, valeur));
     }
-
     @Override
     public void oterSommet(String noeud) {
         arcs.removeIf(arc -> arc.getSource().equals(noeud) || arc.getDestination().equals(noeud));
@@ -72,7 +74,7 @@ public class GrapheLArcs implements IGraphe {
                 return arc.getValuation();
             }
         }
-        throw new IllegalArgumentException("L'arc (" + src + ", " + dest + ") n'existe pas.");
+        return -1; // Renvoie -1 si l'arc n'existe pas.
     }
 
     public boolean contientSommet(String sommet) {
@@ -94,16 +96,23 @@ public class GrapheLArcs implements IGraphe {
         return false;
     }
 
-   @Override
-public String toString() {
-    StringBuilder sb = new StringBuilder();
-    for (Arc arc : arcs) {
-        sb.append(String.format("%s->%s(%d), ", arc.getSource(), arc.getDestination(), arc.getValuation()));
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Arc arc : arcs) {
+            if (arc.getSource().equals(arc.getDestination())) {
+                continue; // Ignore les boucles (arcs ayant la même source et destination)
+            }
+            sb.append(String.format("%s->%s(%d), ", arc.getSource(), arc.getDestination(), arc.getValuation()));
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 2); // Supprime les deux derniers caractères (', ')
+        }
+        return sb.toString();
     }
-    if (sb.length() > 0) {
-        sb.deleteCharAt(sb.length() - 1); //
-    }
-    return sb.toString();
-    }
-  }    
 }
+  
+  
+
+
+
