254a2cbb7f0b05f40d03d28310329fe8889996a2
Bref <115189857+BrefJTF@users.noreply.github.com>
2023-05-21 23:18:18 +0200

    Delete src directory

--- src/main/java/graphe/implems/Graphe.java
+++
@@ -1,8 +0,0 @@
-package main.java.graphe.implems;
-
-import main.java.graphe.core.IGraphe;
-
-public abstract class Graphe implements IGraphe {
-	@Override
-	  public String toString() { return toAString(); }
-}
